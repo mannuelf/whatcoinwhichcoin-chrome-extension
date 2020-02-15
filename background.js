df33266a508b785c054dbba5a332631e9a50aa82
Mannuel Ferreira <mannuel.ferreir@gmail.com>
2020-02-15 18:05:22 +0100

    wip

--- background.js
+++ background.js
@@ -1,5 +1,14 @@
-chrome.runtime.onInstalled.addListener(function() {
-   chrome.storage.sync.set({ color: '#1B98E0'}, function() {
-      console.log("The colour is blue blue blue")
-   });
+chrome.runtime.onInstalled.addListener(function () {
+    chrome.storage.sync.set({color: '#1B98E0'}, function () {
+        console.log("The colour is blue blue blue")
+    });
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+        chrome.declarativeContent.onPageChanged.addRules([{
+            conditions: [new chrome.declarativeContent.PageStateMatcher({
+                pageUrl: {hostEquals: 'developer.chrome.com'},
+            })
+            ],
+            actions: [new chrome.declarativeContent.ShowPageAction()]
+        }]);
+    });
 });
