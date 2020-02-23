055d493829774570624c9d8450c2d84bfb28bc32
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2020-02-23 09:39:52 +0100

    majestic webpack build yay

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,60 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const baseManifest = require('./chrome/manifest.json');
+const WebpackExtensionManifestPlugin = require('webpack-extension-manifest-plugin')
 
-module.exports = {
+const config = {
     mode: 'development',
-    entry: './chrome/index.js',
+    devtool: 'cheap-module-source-map',
+    entry: path.join(__dirname, './static/index.js'),
     output: {
-        filename: 'background.js',
+        filename: '[name].js',
         path: path.resolve(__dirname, 'dist')
+    },
+    resolve: {
+        extensions: ["*", ".js"]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'What Coin Which Coin',
+            meta: {
+                'charset': 'utf-8',
+                'viewport': 'width=device-width, initial-scale=1, shrink-to-fit=no',
+                'theme-color': '#000000'
+            },
+            manifest: 'manifest.json',
+            filename: "index.html",
+            template: './static(index.html',
+            hash: true
+        }),
+        new CopyPlugin([
+            {
+                from: "chrome/icons",
+                to: "icons"
+            }
+        ]),
+        new WebpackExtensionManifestPlugin([{
+            config: baseManifest
+        }])
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"]
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: ["file-loader"]
+            }
+        ]
     }
 };
+
+module.exports = config;
