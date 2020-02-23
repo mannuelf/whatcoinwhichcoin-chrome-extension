7691143b5af09e811817b4ee2a4a10035fa5078d
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2020-02-23 09:26:26 +0100

    refactor

---
+++ app/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class App extends React.Component {
+    render() {
+        return <h1>Hellow World</h1>
+    }
+}
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     mode: 'development',
-    entry: './src/index.js',
+    entry: './chrome/index.js',
     output: {
         filename: 'background.js',
         path: path.resolve(__dirname, 'dist')
