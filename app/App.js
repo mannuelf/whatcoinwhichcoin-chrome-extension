3fd8acdf49e2d6c829457aa6dee0eea9d039b35a
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2020-04-07 17:58:27 +0200

    convert to functional component

--- app/App.js
+++ app/App.js
@@ -1,15 +1,11 @@
-import React from "react";
-import CoinTicker from "./CoinTicker";
+import React, { Component } from 'react';
+import CoinTicker from "./components/CoinTicker";
 import "./App.css";
 
-class App extends React.Component {
-    render() {
-        return (
-            <div className="root container">
-                <CoinTicker />
-            </div>
-        );
-    }
+export default function App() {
+    return (
+        <div className="root container">
+            <CoinTicker />
+        </div>
+    );
 }
-
-export default App;
